afc3b60307c2c6bde1e78f0d7349baa204375132
tema <sezemov2@gmail.com>
2026-05-30 15:06:04 +0300

    feat: added category

---
+++ Models/ClothingCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomClothing.Models;
+
+public class ClothingCategory
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public List<CompletedWork> CompletedWorks { get; set; } = [];
+}
