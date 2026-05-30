48a937b8e71c8b0a7854b07b3bde36e8d38d55f5
tema <sezemov2@gmail.com>
2026-05-30 15:04:41 +0300

    feat: added entities

---
+++ Models/CompletedWorks.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomClothing.Models;
+
+public class CompletedWork
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsPublic { get; set; }
+    public int ClothingCategoryId { get; set; }
+    public ClothingCategory? Category { get; set; }
+}
---
+++ Models/DesignRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomClothing.Models;
+
+public class DesignRequest
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required string CustomerPhone { get; set; }
+    public int ClothingCategoryId { get; set; }
+    public ClothingCategory? Category { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public RequestStatus Status { get; set; } = RequestStatus.Pending;
+}
